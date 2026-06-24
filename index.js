4efc591f54e571b5e167977f4d599025bda0092a
Arpith-RR <arpithrr2006@gmail.com>
2026-06-24 16:48:20 +0530

    github data added

--- index.js
+++ index.js
@@ -4,6 +4,46 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
+const gitHubData = {
+  login: "hiteshchoudhary",
+  id: 11613311,
+  node_id: "MDQ6VXNlcjExNjEzMzEx",
+  avatar_url: "https://avatars.githubusercontent.com/u/11613311?v=4",
+  gravatar_id: "",
+  url: "https://api.github.com/users/hiteshchoudhary",
+  html_url: "https://github.com/hiteshchoudhary",
+  followers_url: "https://api.github.com/users/hiteshchoudhary/followers",
+  following_url:
+    "https://api.github.com/users/hiteshchoudhary/following{/other_user}",
+  gists_url: "https://api.github.com/users/hiteshchoudhary/gists{/gist_id}",
+  starred_url:
+    "https://api.github.com/users/hiteshchoudhary/starred{/owner}{/repo}",
+  subscriptions_url:
+    "https://api.github.com/users/hiteshchoudhary/subscriptions",
+  organizations_url: "https://api.github.com/users/hiteshchoudhary/orgs",
+  repos_url: "https://api.github.com/users/hiteshchoudhary/repos",
+  events_url: "https://api.github.com/users/hiteshchoudhary/events{/privacy}",
+  received_events_url:
+    "https://api.github.com/users/hiteshchoudhary/received_events",
+  type: "User",
+  user_view_type: "public",
+  site_admin: false,
+  name: "Hitesh Choudhary",
+  company: null,
+  blog: "https://hitesh.ai",
+  location: "India",
+  email: null,
+  hireable: null,
+  bio: "I make coding videos on youtube and for courses. My youtube channel explains my work more. Check that out",
+  twitter_username: "hiteshdotcom",
+  public_repos: 123,
+  public_gists: 6,
+  followers: 58253,
+  following: 0,
+  created_at: "2015-03-23T13:03:25Z",
+  updated_at: "2026-06-04T14:16:27Z",
+};
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
@@ -14,4 +54,8 @@ app.get("/twitter", (req, res) => {
 
 app.listen(process.env.PORT, () => {
   console.log(`example app listening on port ${port}`);
+});
+
+app.get("/github", (req, res) => {
+  res.json(gitHubData);
 });
